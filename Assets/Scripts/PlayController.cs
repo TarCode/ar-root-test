e5c0e2804b8f77d265c712fa4448c9ea34224f23
TarCode <tarcode33@gmail.com>
2018-09-18 16:40:16 +0200

    giant commit: destroy laptop and get premium

---
+++ Assets/Scripts/PlayController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        Debug.Log("THIS IS WORKING");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
---
+++ Assets/col.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class col : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("THIS IS HAPPENEING");
+        StartCoroutine(GetText());
+        if (col.gameObject.name == "Sphere")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    IEnumerator GetText()
+    {
+        var apiKey = "sandbox_MWQxNjkxYTktNzI3Yy00OGU3LTg2OGQtZjYxYTQyMTI5NjM5LnozQTJqYWFxbG1CQ25CT1IxNXhlVDZzaVNTS0IwRHBk";
+
+        UnityWebRequest www = UnityWebRequest.Get("https://sandbox.root.co.za/v1");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
